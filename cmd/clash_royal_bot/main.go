98259971d086313cf9d9f20ec44a7cd6aecc26dc
Pooria Kalantari <46835187+pooria1@users.noreply.github.com>
2023-12-04 02:51:30 +0330

    read token from config package

--- cmd/clash_royal_bot/main.go
+++ cmd/clash_royal_bot/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"log"
 
-	"github.com/99pouria/clash-royale-telegram-bot/handler"
+	"github.com/99pouria/clash-royale-telegram-bot/internal/config"
+	"github.com/99pouria/clash-royale-telegram-bot/internal/handler"
 	tbotapi "github.com/99pouria/clash-royale-telegram-bot/pkg/tbot-api"
 )
 
 func main() {
+	// loading config
+	if err := config.LoadConfig(); err != nil {
+		log.Fatal(err)
+	}
 
 	members := tbotapi.Init()
-	updates, err := handler.InitBot("")
+	updates, err := handler.InitBot(config.GetBotToken())
 
 	if err != nil {
 		log.Println(err)
